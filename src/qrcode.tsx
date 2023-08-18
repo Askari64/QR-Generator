b5b58b896d9bf98fb3c9e4c0792e1a5680cce631
Askari <askarir64@gmail.com>
2023-08-18 19:35:58 +0530

    QR Value Dynamic

---
+++ src/qrcode.tsx
@@ -0,0 +1,17 @@
+import QRCode from "react-qr-code"
+
+function Qr(props: { size: number | undefined; value: string }) {
+  return (
+    <div style={{ height: "auto", margin: "0 auto", maxWidth: 250, width: "100%" }}>
+    <QRCode
+    size={props.size}
+    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+    value={props.value}
+    bgColor="#ffffff"
+    fgColor="#000000"
+    />
+    </div>
+  )
+}
+
+export default Qr
