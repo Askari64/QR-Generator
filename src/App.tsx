4eb7d328d763bc6c3351f590292ceeeb2083b162
Askari <askarir64@gmail.com>
2023-08-20 18:43:53 +0530

    changed styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import Qr from './qrcode'
-import { useState } from 'react'
-import './App.css'
+import Qr from "./qrcode";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
 
   return (
     <>
-    <input type='string'
-    placeholder='Enter Value' 
-    onChange={(e) => {
-      setValue(e.target.value)
-    }}/>
-    <Qr size={500} value={value}/>
+    <section>
+        <h1>QR Generator</h1>
+    <p>Enter the text you want to generate a <span id="highlight">QR Code</span> for.</p>
+    <p><span id="info">Information</span> - Maximum input value can not exceed 2036 characters</p>
+    </section>
+  
+    <div className="InputContainer">
+    <input
+      className="input"
+        type="string"
+        placeholder="Enter Value"
+        maxLength={2036}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+    </div>
+      
+      <Qr size={500} value={value} />
+
+      <button className="button" ><span>Download</span></button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/qrcode.tsx
+++ src/qrcode.tsx
@@ -1,17 +1,19 @@
-import QRCode from "react-qr-code"
+import QRCode from "react-qr-code";
 
 function Qr(props: { size: number | undefined; value: string }) {
   return (
-    <div style={{ height: "auto", margin: "0 auto", maxWidth: 250, width: "100%" }}>
-    <QRCode
-    size={props.size}
-    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-    value={props.value}
-    bgColor="#ffffff"
-    fgColor="#000000"
-    />
+    <div
+      style={{ height: "auto", margin: "0 auto", maxWidth: 150, width: "100%" }}
+    >
+      <QRCode
+        size={props.size}
+        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+        value={props.value}
+        bgColor="#ffffff"
+        fgColor="#000000"
+      />
     </div>
-  )
+  );
 }
 
-export default Qr
+export default Qr;
